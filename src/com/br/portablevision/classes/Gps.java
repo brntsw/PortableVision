1c79ab3ea9e34c3847a09387111c3b2d9d50435c
Bruno Pardini <bruno.pardini6@gmail.com>
2014-04-02 15:06:23 -0300

    Revert "General commit"
    This reverts commit 6bfe8acfcd0b5a8cc3d16027dd3d027cc1255130.

--- src/com/br/portablevision/classes/Gps.java
+++
@@ -1,5 +0,0 @@
-package com.br.portablevision.classes;
-
-public class Gps {
-
-}
--- src/com/br/portablevision/persistente/IDescricaoDAO.java
+++
@@ -1,7 +0,0 @@
-package com.br.portablevision.persistente;
-
-public interface IDescricaoDAO {
-	public void cadastrarAtivo();
-	public int alterarAtivo();
-	public int consultarAtivo();
-}
